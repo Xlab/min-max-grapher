b5a624e88cb8814abc2d1a15eadc79528cc98af0
Max Kouprianov <me@kc.vc>
2012-01-18 00:58:51 +0400

    Initial commit

---
+++ src/minmax/MinMaxGrapher.java
@@ -0,0 +1,12 @@
+package minmax;
+
+/**
+ *
+ * @author Kouprianov Maxim <me@kc.vc> @ SE HSE
+ */
+public class MinMaxGrapher {
+    public static void main(String[] args) {
+        new MainForm().setVisible(true);
+    }
+
+}
---
+++ src/minmax/gui/Grid.java
@@ -0,0 +1,13 @@
+package minmax.gui;
+
+/**
+ *
+ * @author Kouprianov Maxim <me@kc.vc> @ SE HSE
+ */
+public class Grid {
+
+    public Grid(int size, int max) {
+        
+    }
+
+}
